1f8e4e3500ca1453544e77fa52d6781a70fa6299
Cường <Buicuong288@gmail.com>
2024-03-13 23:07:21 +0700

    edit

--- lib/article_model.dart
+++ lib/article_model.dart
@@ -1,68 +1,29 @@
-// import 'package:shared_preferences/shared_preferences.dart';
-// import 'dart:convert';
-// import 'package:flutter/material.dart';
-// class Article {
-//   final String title;
-//   final String urlToImage;
-//   final String content;
-//   final String source;
-//   final String publishedAt;
 
-//   Article({
-//     required this.title,
-//     required this.urlToImage,
-//     required this.content,
-//     required this.source,
-//     required this.publishedAt,
-//   });
+class Article {
+  final String title;
+  final String urlToImage;
+  final String content;
+  final String source;
+  final String publishedAt;
+  final String category;
 
-//   Map<String, dynamic> toJson() {
-//     return {
-//       'title': title,
-//       'urlToImage': urlToImage,
-//       'content': content,
-//       'source': source,
-//       'publishedAt': publishedAt,
-//     };
-//   }
+  Article({
+    required this.title,
+    required this.urlToImage,
+    required this.content,
+    required this.source,
+    required this.publishedAt,
+    required this.category,
+  });
 
-//   factory Article.fromJson(Map<String, dynamic> json) {
-//     return Article(
-//       title: json['title'] ?? '',
-//       urlToImage: json['urlToImage'] ?? '',
-//       content: json['content'] ?? '',
-//       source: json['source']['name'] ?? '',
-//       publishedAt: json['publishedAt'] ?? '',
-//     );
-//   }
-// }
-
-// extension ArticleListExtension on List<Article> {
-//   List<Map<String, dynamic>> toJsonList() {
-//     return this.map((article) => article.toJson()).toList();
-//   }
-// }
-
-// extension JsonListExtension on List<Map<String, dynamic>> {
-//   List<Article> toArticleList() {
-//     return this.map((json) => Article.fromJson(json)).toList();
-//   }
-// }
-
-// class ArticleStorage {
-//   Future<void> saveArticles(List<Article> articles) async {
-//     final prefs = await SharedPreferences.getInstance();
-//     final savedArticles = prefs.getStringList('saved_articles') ?? [];
-
-//     savedArticles.addAll(articles.toJsonList().map((json) => jsonEncode(json)));
-
-//     await prefs.setStringList('saved_articles', savedArticles);
-//   }
-
-//   Future<List<Article>> getSavedArticles() async {
-//     final prefs = await SharedPreferences.getInstance();
-//     final savedArticles = prefs.getStringList('saved_articles') ?? [];
-
-//     return savedArticles.map((articleJson) => json.decode(articleJson)).toList().toArticleList();
-//   }
-// }
+  factory Article.fromJson(Map<String, dynamic> json) {
+    return Article(
+      title: json['title'] ?? '',
+      urlToImage: json['urlToImage'] ?? '',
+      content: json['content'] ?? '',
+      source: json['source']['name'] ?? '',
+      publishedAt: json['publishedAt'] ?? '',
+      category: json['category'] ?? '',  // Thêm lĩnh vực
+    );
+  }
+}
--- lib/home_screen.dart
+++ lib/home_screen.dart
@@ -3,35 +3,7 @@ import 'package:cached_network_image/cached_network_image.dart';
 import 'package:intl/intl.dart';
 import 'package:http/http.dart' as http;
 import 'dart:convert';
-
-class Article {
-  final String title;
-  final String urlToImage;
-  final String content;
-  final String source;
-  final String publishedAt;
-  final String category;
-
-  Article({
-    required this.title,
-    required this.urlToImage,
-    required this.content,
-    required this.source,
-    required this.publishedAt,
-    required this.category,
-  });
-
-  factory Article.fromJson(Map<String, dynamic> json) {
-    return Article(
-      title: json['title'] ?? '',
-      urlToImage: json['urlToImage'] ?? '',
-      content: json['content'] ?? '',
-      source: json['source']['name'] ?? '',
-      publishedAt: json['publishedAt'] ?? '',
-      category: json['category'] ?? '',  // Thêm lĩnh vực
-    );
-  }
-}
+import 'article_model.dart';
 
 class HomeScreen extends StatefulWidget {
   const HomeScreen({Key? key}) : super(key: key);
